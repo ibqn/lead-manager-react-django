3a0f8d6777ca67736368fcdc1d1712f8efa929b7
Evgeny Bobkin <evgen.ibqn@gmail.com>
2019-09-16 08:54:03 +0200

    remove all

--- leads/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import routers
-from leads.views import LeadViewSet
-
-router = routers.DefaultRouter()
-router.register('leads', LeadViewSet, 'leads')
-
-urlpatterns = router.urls
